5b546080212ddbf819c922a354475578470a1432
Matt Evans <mevans93308@gmail.com>
2016-02-21 21:08:37 -0800

    added html template to views.py

--- app/views.py
+++ app/views.py
@@ -4,4 +4,14 @@ from app import App
 @App.route('/')
 @App.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Matt'}
+    return '''
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['nickname'] + '''</h1>
+    <body>
+</html>
+'''

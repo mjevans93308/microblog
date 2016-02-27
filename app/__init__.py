f89ca834beba5f917d8126b28fe9e3dc884867b5
Matt Evans <mevans93308@gmail.com>
2016-02-26 22:06:21 -0800

    implemented user tracking and login, logout functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
 
 App = Flask(__name__)
 App.config.from_object('config')
 db = SQLAlchemy(App)
 
+lm = LoginManager()
+lm.init_app(App)
+lm.login_view = 'login'
+oid = OpenID(App, os.path.join(basedir, 'tmp'))
+
 from app import views, models
+
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 
 from app import App
-App.run(debug=True)
+App.run(debug = True)
